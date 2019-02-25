aa3e0f27aaea9b6148eddd086f9baff3c8200b9d
Simon Hoyos <shmesa23@gmail.com>
2019-02-25 10:39:34 -0500

    Render app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Nav } from "./components/Nav";
+import { Section } from './components/Section';
+import { Home } from "./components/Home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav />
+        <main>
+          <Home />
+          <Section
+            title="experience"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quasi ducimus iste quod labore, praesentium harum, amet neque doloribus cumque atque qui vero perferendis unde fugit et. Pariatur, dolores similique!"
+          />
+          <Section
+            title="projects"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quasi ducimus iste quod labore, praesentium harum, amet neque doloribus cumque atque qui vero perferendis unde fugit et. Pariatur, dolores similique!"
+          />
+        </main>
       </div>
     );
   }
