d284f66b632e87a75f3d86c105389006ea372f75
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 11:54:45 -0500

    Add side projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import { Section } from "../components/Section";
-import { Card } from "../components/Card";
-import { Grid } from "../components/Grid";
+import { Section } from '../components/Section';
+import { Card } from '../components/Card';
+import { Grid } from '../components/Grid';
+import reactmde from '../assets/reactmde.png';
+import make from "../assets/make.jpg";
 
 export function Projects() {
   return (
     <Section>
       <h2>projects</h2>
+      <h3>Training and Mentoring</h3>
       <Grid>
-        Coming soon...
+        <Card
+          title="React Medellin"
+          description="A group of people interested in learning React and its ecosystem."
+          image={{
+            src: reactmde,
+            alt: 'React Medellin Meetup logo'
+          }}
+          website="https://www.meetup.com/React-Medellin/"
+        />
+        <Card
+          title="Make it Real Top"
+          description="High quality online and live programming mentoring."
+          image={{
+            src: make,
+            alt: 'make it real logo'
+          }}
+          website="https://makeitreal.camp/top"
+        />
+      </Grid>
+      <h3>Build With React</h3>
+      <Grid>
+        <Card
+          title="React Medellin"
+          description="A group of people interested in learning React and its ecosystem."
+          image={{
+            src: reactmde,
+            alt: 'React Medellin Meetup logo'
+          }}
+          website="https://www.meetup.com/React-Medellin/"
+        />
+        <Card
+          title="Make it Real Top"
+          description="High quality online and live programming mentoring."
+          image={{
+            src: make,
+            alt: 'make it real logo'
+          }}
+          website="https://makeitreal.camp/top"
+        />
       </Grid>
     </Section>
   );
