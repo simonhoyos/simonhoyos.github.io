3879be794b71b3be1ad74df7d988585f89f45320
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 07:39:22 -0500

    Rename project card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import stairs from '../assets/stairs.jpg';
 
-const Card = styled.article`
+const StyledCard = styled.article`
   width: 350px;
   height: auto;
   box-shadow: 1px 1px 3px 1px lightgrey;
@@ -11,6 +11,7 @@ const Card = styled.article`
 
 const Image = styled.img`
   width: 100%;
+  height: 350px;
   object-fit: cover;
   border-radius: 10px 10px 0 0;
 `;
@@ -20,14 +21,14 @@ const Content = styled.div`
   padding: 10px;
 `;
 
-export function ProjectCard() {
+export function Card() {
   return (
-    <Card>
+    <StyledCard>
       <Image src={stairs} alt=""/>
       <Content>
         <h3>Project Card</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem quasi, ut totam placeat labore</p>
       </Content>
-    </Card>
+    </StyledCard>
   )
 }
