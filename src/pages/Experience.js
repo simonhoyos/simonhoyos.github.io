851de32ba69b118e879b6cee5641b290d0dd88fa
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 10:15:46 -0500

    Add experiences

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,17 +2,42 @@ import React from 'react';
 import { Section } from '../components/Section';
 import { Card } from "../components/Card";
 import { Grid } from "../components/Grid";
+import huge from "../assets/huge.png";
+import magneto from "../assets/magneto.jpg";
+import make from "../assets/make.jpg";
 
 export function Experience() {
   return (
     <Section>
       <h2>experience</h2>
       <Grid>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card
+          title="Huge Inc."
+          description="Huge is an experience design and digital marketing agency that provides strategy, marketing, design, and technology services to Fortune 100 companies."
+          image={{
+            src: huge,
+            alt: 'huge logo'
+          }}
+          website="https://www.hugeinc.com/"
+        />
+        <Card
+          title="Magneto365"
+          description="Find and apply to inspiring jobs"
+          image={{
+            src: magneto,
+            alt: 'magneto logo'
+          }}
+          website="https://www.magneto365.com/"
+        />
+        <Card
+          title="Make it Real"
+          description="High quality online and live programming mentoring"
+          image={{
+            src: make,
+            alt: 'make it real logo'
+          }}
+          website="https://makeitreal.camp/"
+        />
       </Grid>
     </Section>
   );
