c69845792e1e6f2ee51aad8426f2129572e589a3
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 11:54:34 -0500

    Update experiences

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -5,11 +5,15 @@ import { Grid } from "../components/Grid";
 import huge from "../assets/huge.png";
 import magneto from "../assets/magneto.jpg";
 import make from "../assets/make.jpg";
+import vantty from '../assets/vantty.png';
 
 export function Experience() {
   return (
     <Section>
       <h2>experience</h2>
+      <h3>Employee</h3>
+      <div>
+
       <Grid>
         <Card
           title="Huge Inc."
@@ -19,7 +23,7 @@ export function Experience() {
             alt: 'huge logo'
           }}
           website="https://www.hugeinc.com/"
-        />
+          />
         <Card
           title="Magneto365"
           description="Find and apply to inspiring jobs."
@@ -28,7 +32,7 @@ export function Experience() {
             alt: 'magneto logo'
           }}
           website="https://www.magneto365.com/"
-        />
+          />
         <Card
           title="Make it Real"
           description="High quality online and live programming mentoring."
@@ -37,6 +41,20 @@ export function Experience() {
             alt: 'make it real logo'
           }}
           website="https://makeitreal.camp/"
+          />
+      </Grid>
+      </div>
+      <h3>Entrepreneurship</h3>
+      <Grid>
+        <Card
+          title="Vantty"
+          description="Make up artist marketplace. Connect clients with top make up artists."
+          image={{
+            src: vantty,
+            alt: 'vantty logo'
+          }}
+          imageSize="contain"
+          website="https://www.vantty.com/"
         />
       </Grid>
     </Section>
