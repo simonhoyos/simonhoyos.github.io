4b0db22c53e98a27886a516425178d155be75842
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 16:08:32 -0500

    Add Y scroll to cards

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const Grid = styled.div`
   display: grid;
   grid-gap: 15px;
-  grid-template-columns: repeat(auto-fit, 350px);
+  grid-template-columns: repeat(auto-fill, 350px);
+  grid-auto-flow: column;
 `;
