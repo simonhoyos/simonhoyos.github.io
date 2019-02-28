b865f4e148b54776dc57a160fa31b1c960a13b66
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 09:17:03 -0500

    Social links in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProfilePicture } from '../components/ProfilePicture';
+import { Icon } from "./Icon";
 
 const StyledNav = styled.nav`
-  height: 100vh;
+  min-height: 100vh;
   width: 250px;
   margin: 0;
   padding: 0;
-  position: fixed;
   display: none;
 
   @media screen and (min-width: 960px) {
@@ -26,22 +26,11 @@ const StyledNav = styled.nav`
     height: 60px;
     margin: 0;
     padding: 10px;
-    width: 100%;
     position: relative;
     text-align: center;
     text-transform: uppercase;
     font-weight: bold;
     padding: 0;
-
-    &:after {
-      width: calc(100% - 200px);
-      height: 1px;
-      background-color: #333;
-      content: '';
-      position: absolute;
-      bottom: 0;
-      left: 100px;
-    }
   }
 
   a {
@@ -58,17 +47,62 @@ const StyledNav = styled.nav`
       background-color: #f5f5f5;
     }
   }
+
+  .nav li {
+    &:after {
+      width: calc(100% - 200px);
+      height: 1px;
+      background-color: #333;
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 100px;
+    }
+  }
+
+  .social {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .social li {
+    display: inline-block;
+    width: 25%;
+  }
 `;
 
 export function Nav() {
   return (
     <StyledNav>
       <ProfilePicture />
-      <ul>
+      <ul className="nav">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/experience">Experience</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/blog">Blog</Link></li>
+      </ul>
+
+      <ul className="social">
+        <li>
+          <a href="https://www.github.com/simonhoyos" rel="noopener noreferrer" target="_blank">
+            <Icon icon="github" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/simonhoyosdev" rel="noopener noreferrer" target="_blank">
+            <Icon icon="twitter" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.medium.com/@simonhoyos" rel="noopener noreferrer" target="_blank">
+            <Icon icon="medium" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/simonhoyos" rel="noopener noreferrer" target="_blank">
+            <Icon icon="linkedin" />
+          </a>
+        </li>
       </ul>
     </StyledNav>
   );
