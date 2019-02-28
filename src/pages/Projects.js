f848d9048c1c9810a1bd99ba64847cee93e42ccb
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 09:17:30 -0500

    Show cards in projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Section } from "../components/Section";
+import { Card } from "../components/Card";
+import { Grid } from "../components/Grid";
 
 export function Projects() {
   return (
     <Section>
       <h2>projects</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quasi ducimus iste quod labore, praesentium harum, amet neque doloribus cumque atque qui vero perferendis unde fugit et. Pariatur, dolores similique!</p>
+      <Grid>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </Grid>
     </Section>
   );
 }
