3879e0d6baf282b4fedbd84369a3aa1246de2902
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 11:55:26 -0500

    Update grid styles

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,6 +3,5 @@ import styled from 'styled-components';
 export const Grid = styled.div`
   display: grid;
   grid-gap: 15px;
-  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
-  justify-items: center;
+  grid-template-columns: repeat(auto-fit, 350px);
 `;
