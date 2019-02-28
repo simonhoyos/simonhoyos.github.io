f03b22988b61a69cee7d0476fb4314e4ce312938
Simon Hoyos <shmesa23@gmail.com>
2019-02-28 16:06:48 -0500

    Loop data

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,61 +2,32 @@ import React from 'react';
 import { Section } from '../components/Section';
 import { Card } from "../components/Card";
 import { Grid } from "../components/Grid";
-import huge from "../assets/huge.png";
-import magneto from "../assets/magneto.jpg";
-import make from "../assets/make.jpg";
-import vantty from '../assets/vantty.png';
+import { CardContainer } from '../components/CardContainer';
+import { SectionHeader } from "../components/SectionHeader";
+import { experiences } from '../data/experiences';
 
 export function Experience() {
   return (
     <Section>
       <h2>experience</h2>
-      <h3>Employee</h3>
-      <div>
-
-      <Grid>
-        <Card
-          title="Huge Inc."
-          description="Experience design and digital marketing agency that provides strategy, marketing, design, and technology services."
-          image={{
-            src: huge,
-            alt: 'huge logo'
-          }}
-          website="https://www.hugeinc.com/"
-          />
-        <Card
-          title="Magneto365"
-          description="Find and apply to inspiring jobs."
-          image={{
-            src: magneto,
-            alt: 'magneto logo'
-          }}
-          website="https://www.magneto365.com/"
-          />
-        <Card
-          title="Make it Real"
-          description="High quality online and live programming mentoring."
-          image={{
-            src: make,
-            alt: 'make it real logo'
-          }}
-          website="https://makeitreal.camp/"
-          />
-      </Grid>
-      </div>
-      <h3>Entrepreneurship</h3>
-      <Grid>
-        <Card
-          title="Vantty"
-          description="Make up artist marketplace. Connect clients with top make up artists."
-          image={{
-            src: vantty,
-            alt: 'vantty logo'
-          }}
-          imageSize="contain"
-          website="https://www.vantty.com/"
-        />
-      </Grid>
+      {experiences.map(({ section, cards}) => (
+        <>
+          <SectionHeader>{section}</SectionHeader>
+          <CardContainer>
+            <Grid>
+              {cards.map(({ title, subtitle, description, image, icons}) => (
+                <Card
+                  title={title}
+                  subtitle={subtitle}
+                  description={description}
+                  image={image}
+                  icons={icons}
+                />
+              ))}
+            </Grid>
+          </CardContainer>
+        </>
+      ))}
     </Section>
   );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,55 +2,32 @@ import React from 'react';
 import { Section } from '../components/Section';
 import { Card } from '../components/Card';
 import { Grid } from '../components/Grid';
-import reactmde from '../assets/reactmde.png';
-import make from "../assets/make.jpg";
+import { CardContainer } from '../components/CardContainer';
+import { SectionHeader } from '../components/SectionHeader';
+import { projects } from '../data/projects';
 
 export function Projects() {
   return (
     <Section>
       <h2>projects</h2>
-      <h3>Training and Mentoring</h3>
-      <Grid>
-        <Card
-          title="React Medellin"
-          description="A group of people interested in learning React and its ecosystem."
-          image={{
-            src: reactmde,
-            alt: 'React Medellin Meetup logo'
-          }}
-          website="https://www.meetup.com/React-Medellin/"
-        />
-        <Card
-          title="Make it Real Top"
-          description="High quality online and live programming mentoring."
-          image={{
-            src: make,
-            alt: 'make it real logo'
-          }}
-          website="https://makeitreal.camp/top"
-        />
-      </Grid>
-      <h3>Build With React</h3>
-      <Grid>
-        <Card
-          title="React Medellin"
-          description="A group of people interested in learning React and its ecosystem."
-          image={{
-            src: reactmde,
-            alt: 'React Medellin Meetup logo'
-          }}
-          website="https://www.meetup.com/React-Medellin/"
-        />
-        <Card
-          title="Make it Real Top"
-          description="High quality online and live programming mentoring."
-          image={{
-            src: make,
-            alt: 'make it real logo'
-          }}
-          website="https://makeitreal.camp/top"
-        />
-      </Grid>
+      {projects.map(({ section, cards}) => (
+        <>
+          <SectionHeader>{section}</SectionHeader>
+          <CardContainer>
+            <Grid>
+              {cards.map(({ title, subtitle, description, image, icons}) => (
+                <Card
+                  title={title}
+                  subtitle={subtitle}
+                  description={description}
+                  image={image}
+                  icons={icons}
+                />
+              ))}
+            </Grid>
+          </CardContainer>
+        </>
+      ))}
     </Section>
   );
 }
