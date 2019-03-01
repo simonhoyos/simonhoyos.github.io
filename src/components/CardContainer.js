d1c8aea507f722453314411ed03d5097fc869cdf
Simon Hoyos <shmesa23@gmail.com>
2019-03-01 11:28:53 -0500

    Show/hide scroll message

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,6 +1,44 @@
+import React, { Component, createRef } from 'react';
 import styled from 'styled-components';
+import { SectionHeader } from "./SectionHeader";
+import { Grid } from "../components/Grid";
 
-export const CardContainer = styled.div`
-  overflow: auto;
+const Container = styled.div`
+  overflow: scroll;
   padding: 5px;
+  -webkit-overflow-scrolling: touch;
 `;
+
+class CardContainer extends Component {
+  state = {
+    isScrollable: false,
+  }
+
+  scrollableRef = createRef();
+
+  componentDidMount() {
+    const { scrollWidth, offsetWidth } = this.scrollableRef.current;
+    const isScrollable = scrollWidth > offsetWidth + 15;
+
+    this.setState({
+      isScrollable,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <SectionHeader isScrollable={this.state.isScrollable}>
+          {this.props.title}
+        </SectionHeader>
+        <Container>
+          <Grid ref={this.scrollableRef}>
+            {this.props.children}
+          </Grid>
+        </Container>
+      </>
+    );
+  }
+}
+
+export { CardContainer };
